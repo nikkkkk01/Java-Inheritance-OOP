75af1921d0ba3f3edfe7fc882d827fab82e66dcb
n1k <alexaguel367@gmail.com>
2025-09-05 17:10:48 +0800

    Add BankAccount class with deposit and balance methods

---
+++ BankAccount.java
@@ -0,0 +1,17 @@
+package myPackage;
+
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
